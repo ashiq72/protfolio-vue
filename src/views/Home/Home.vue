30478cce198922562a4473b5f3907f9e2d88b043
ashiq72 <ahmedashik18k@gmail.com>
2024-09-04 11:59:14 +0600

    Updated scroll

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -6,7 +6,22 @@
     <Services />
     <!-- <Contact /> -->
     <Testimonial />
+
     <Patner />
+
+
+    <div>
+      <div ref="splide" class="splide scrollbar-hide ">
+        <div class="splide__track">
+          <ul class="splide__list">
+            <li class="splide__slide">Slide 1</li>
+            <li class="splide__slide">Slide 2</li>
+            <li class="splide__slide">Slide 3</li>
+            <!-- Add more slides as needed -->
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
@@ -16,4 +31,27 @@ import Services from "../../components/Services.vue";
 import Soluations from "../../components/Solutions.vue";
 import Testimonial from "../../components/Testimonial.vue";
 import Patner from "../../components/Patner.vue";
+
+import { ref, onMounted } from 'vue';
+import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+const splide = ref(null);
+
+onMounted(() => {
+  const splideInstance = new Splide(splide.value, {
+    type: 'loop',
+    drag: 'free',
+    focus: 'center',
+    perPage: 3,
+    autoScroll: {
+      speed: 1,
+    },
+  });
+
+  splideInstance.mount({ AutoScroll });
+});
+
+
+
 </script>
